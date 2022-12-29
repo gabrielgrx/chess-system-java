5c2a5cc2472453fc167debdfe9d3a257900e8e44
Gabriel Xavier <gabrielgrx@gmail.com>
2022-12-28 23:52:15 -0300

    Switching payer each turn

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,3 +3,5 @@ D
 src/chess/Color.java,6\f\6f5048016a57bbf452eb3bb3965bb62dfb909033
 Q
 !src/boardgame/BoardException.java,c\d\cd20c15b434e2bd9d33b559985a9b393401fdb2d
+L
+src/chess/ChessPosition.java,9\0\90a7f058c20369970d1f5d1c161ad421280924ef
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,5 +1,6 @@
 package application;
 
+import chess.ChessMatch;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -47,6 +48,13 @@ public class UI {
         }
     }
 
+    public static void printMatch(ChessMatch chessMatch) {
+        printBoard(chessMatch.getPieces());
+        System.out.println();
+        System.out.println("Turn: " + chessMatch.getTurn());
+        System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+    }
+
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -70,7 +78,7 @@ public class UI {
     }
 
     private static void printPiece(ChessPiece piece, boolean background) {
-        if (background){
+        if (background) {
             System.out.print(ANSI_BLUE_BACKGROUND);
         }
         if (piece == null) {

8a880b1e040b7066fc740197f9d43bec4ad7c44a
Gabriel Xavier <gabrielgrx@gmail.com>
2022-12-29 15:19:06 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -17,6 +17,18 @@ public abstract class ChessPiece extends Piece {
         return color;
     }
 
+    public int getMoveCount(){
+        return moveCount;
+    }
+
+    public void increaseMoveCount(){
+        moveCount ++;
+    }
+
+    public void decreaseMoveCount(){
+        moveCount --;
+    }
+
     public ChessPosition getChessPosition(){
         return ChessPosition.fromPosition(position);
     }
